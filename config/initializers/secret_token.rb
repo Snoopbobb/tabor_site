4687cc1ba83fae687d275b61416acc8e099038fe
Snoopbobb <snoopbobb@q.com>
2013-10-18 20:19:53 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TaborSite::Application.config.secret_token = '2f005e7165b0c5eafe485ed7dcdc162924e24e6195514431e68d8571e7bee1bca9062656410c12697d280ed56fcd7c5c60b909bb38d1522a89761a37e8b01447'
