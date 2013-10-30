4c54937de3526c7fb64e208435bc99b30208b604
Snoopbobb <snoopbobb@q.com>
2013-10-29 18:28:15 -0700

    postmark api key

--- config/application.rb
+++ config/application.rb
@@ -67,7 +67,7 @@ module TaborSite
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
-    config.action_mailer.delivery_method   = :postmark
-    config.action_mailer.postmark_settings = { :api_key => ['bb07a470-6503-477b-8482-6f8dcfc948c3'] }
+    config.action_mailer.delivery_method = :postmark
+    config.action_mailer.postmark_settings = { :api_key => "bb07a470-6503-477b-8482-6f8dcfc948c3" }
   end
 end
